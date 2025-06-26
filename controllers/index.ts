957779bac7efd8a43c9c39d25600223d52fdad1a
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-06-26 21:12:01 +0100

    update tsconfig

--- controllers/index.ts
+++ controllers/index.ts
@@ -3,7 +3,7 @@ import expressAsyncHandler from 'express-async-handler';
 import { Request, Response } from 'express';
 
 export const textToSpeech = expressAsyncHandler(async (req: Request, res: Response) => {
-  const { text, lang = 'en' } = req.body;
+  const { text} = req.query;
   if (!text) {
     res.status(400).json({ success: false, message: 'Text is required' });
     return;
