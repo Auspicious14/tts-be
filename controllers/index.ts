f755597fb2903107e4398dbf7b993ae76c80418b
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-06-26 21:09:33 +0100

    add better node gtts

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,4 +1,4 @@
-import gtts from 'node-gtts';
+import gtts from 'better-node-gtts';
 import expressAsyncHandler from 'express-async-handler';
 import { Request, Response } from 'express';
 
@@ -8,7 +8,7 @@ export const textToSpeech = expressAsyncHandler(async (req: Request, res: Respon
     res.status(400).json({ success: false, message: 'Text is required' });
     return;
   }
-  const gttsInstance = gtts(lang);
+  const gttsInstance = gtts().default;
   res.set({
     'Content-Type': 'audio/mpeg',
     'Content-Disposition': 'inline; filename="tts.mp3"'
--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import { textToSpeech } from './controllers';
+import { textToSpeech } from '../controllers';
 
-const router = express().Router();
+const router = express.Router();
 
 router.post('/text-to-speech', textToSpeech)
 export default router;
