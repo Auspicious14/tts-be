c057459170cc9dd5e6d5e51b63dd7775da99d595
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-06-27 12:00:50 +0100

    chore: add available voice router endpoint

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { textToSpeech, textToSpeechWithEdge } from '../controllers';
+import { textToSpeech, textToSpeechWithEdge, getAvailableVoicesInEdge } from '../controllers';
 
 const router = express.Router();
 
 router.get('/text-to-speech', textToSpeech)
 router.get('/text-to-speech-with-edge', textToSpeechWithEdge)
+router.get('/get-available-voices', getAvailableVoicesInEdge)
 
 export default router;
   
