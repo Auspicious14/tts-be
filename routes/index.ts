3e92233019925f6b01b446d1ef8322c6fa3bb6a7
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-06-27 11:34:28 +0100

    Update textToSpeechWithEdge endpoint

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { textToSpeech } from '../controllers';
+import { textToSpeech, textToSpeechWithEdge } from '../controllers';
 
 const router = express.Router();
 
 router.get('/text-to-speech', textToSpeech)
+router.get('/text-to-speech-with-edge', textToSpeechWithEdge)
+
 export default router;
   
