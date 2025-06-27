b1a442f5114ef6477698dfd1100c79f3377e559e
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-06-27 10:19:03 +0100

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -3,6 +3,6 @@ import { textToSpeech } from '../controllers';
 
 const router = express.Router();
 
-router.post('/text-to-speech', textToSpeech)
+router.get('/text-to-speech', textToSpeech)
 export default router;
   
