37f0dccf2465b6225f71c9e666c56cb1fa5fed97
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-07-07 20:46:13 +0100

    remove unused log

--- services/extractText.ts
+++ services/extractText.ts
@@ -4,9 +4,7 @@ import { franc } from "franc";
 export const extractTextFromImage = async (
   imagePath: string
 ): Promise<{ text: string; lang: string }> => {
-  const { data } = await Tesseract.recognize(imagePath, "eng+fra+ara+yor+deu", {
-    logger: (m) => console.log(m), // optional
-  });
+  const { data } = await Tesseract.recognize(imagePath, "eng+fra+ara+yor+deu");
 
   const rawText = data.text.trim();
 
